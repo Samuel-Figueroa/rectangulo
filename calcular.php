a2737cded57a5d401f6cf2ed0a4c32189167b5b0
Samuel-Figueroa <77761164+Samuel-Figueroa@users.noreply.github.com>
2021-06-30 17:17:08 -0400

    Add files via upload

--- calcular.php
+++ calcular.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
-</head>
-<body>
-<div class="container">
-    <div>
-    <?php
+<?php
 $cateto1 = 3;
 $cateto2= 4;
 $hipo = null;
@@ -24,9 +12,6 @@ echo "La hipotenusa es: ".$hipo;
 ;
 
 ?>  
-    
-    </div>
-</div>
 
 </body>
 </html>
